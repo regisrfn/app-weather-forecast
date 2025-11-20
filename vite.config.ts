96d6dafcd99ef628d189993d7080dff9ab22b880
Your Name <your.email@example.com>
2025-11-20 13:26:07 -0300

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, // Aceita conexões de qualquer IP da rede
+    port: 5173,
+    // Opcional: configurar strictPort para não mudar a porta se estiver ocupada
+    strictPort: false,
+  }
 })
