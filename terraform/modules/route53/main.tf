36f0d0b0f0077d1caf7ddd9255659172fbac8f69
regisrfn <regis.rfnrodrigues@gmail.com>
2025-12-09 19:24:44 -0300

    feat: add email configuration records for ImprovMX including MX, SPF, and DMARC

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -29,3 +29,44 @@ resource "aws_route53_record" "caa" {
     "0 issue \"amazonaws.com\""
   ]
 }
+
+# ==============================================================================
+# EMAIL CONFIGURATION (ImprovMX)
+# ==============================================================================
+
+# MX Records - Email forwarding via ImprovMX
+resource "aws_route53_record" "mx" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "MX"
+  ttl     = 3600
+
+  records = [
+    "10 mx1.improvmx.com",
+    "20 mx2.improvmx.com"
+  ]
+}
+
+# SPF Record - Sender Policy Framework
+resource "aws_route53_record" "spf" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = var.domain_name
+  type    = "TXT"
+  ttl     = 3600
+
+  records = [
+    "v=spf1 include:spf.improvmx.com ~all"
+  ]
+}
+
+# DMARC Record - Domain-based Message Authentication
+resource "aws_route53_record" "dmarc" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = "_dmarc.${var.domain_name}"
+  type    = "TXT"
+  ttl     = 3600
+
+  records = [
+    "v=DMARC1; p=none; rua=mailto:contato@${var.domain_name}"
+  ]
+}
